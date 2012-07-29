31de83b6714929aa7369a22869b3077b8a6357e2
Yuval Karmi <yuval@Yuvals-MacBook-Pro.local>
2012-07-28 19:18:18 -0700

    initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Whydidthechicken::Application.config.secret_token = '6cec594bb04e6f01da93a4d5cccdb01ab1affdd8551449ec995d54b7b4bb7322e249e8fa6d7d4b6cb492c9ae4043c1af04d21e4c434e4f73f37db0231f96a822'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Whydidthechicken::Application.config.session_store :cookie_store, key: '_whydidthechicken_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Whydidthechicken::Application.config.session_store :active_record_store
